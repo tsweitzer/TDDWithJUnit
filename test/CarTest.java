fb602d2a56a79f7daa93c1864f7202ba67c771bd
Teia Sweitzer <teiasweitzer@teias-air.eau.wi.charter.com>
2016-01-29 16:06:58 -0600

    Wrote test for deleration

--- test/CarTest.java
+++ test/CarTest.java
@@ -32,6 +32,11 @@ public class CarTest {
 	}
 
 	// Create and pass test for deceleration.
+	@Test
+	public void canDecelerate(){
+		Car car1 = new Car("Red", 100);
+		car1.decelerate(50);
+	}
 	
 	
 	// Create and pass test for no deceleration below 0.
